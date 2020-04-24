879d63603895ae42b649382c7a3aa42331a4d174
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2020-04-24 14:25:42 -0300

    Finalizado back-end

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SemanaOmniStack11
-Rocket Seat- Semana OmniStack 11 - Projeto "Be The Hero"
+Semana OmniStack 11 - Rocket Seat - Projeto "Be The Hero"
+
+## Estrutura do projeto
+
+| Pasta | Descrição |
+| ----------- | ----------- |
+| ./backend | API REST feita em Express conectando o mobile e o front-end ao banco de dados SQLite |
+| ./frontend | Interface web RectJS para cadastro de Devs |
+| ./mobile | Interface mobile para consultar os Devs cadastrados |
+
+## Entidades
+ * ONG
+ * Caso (Incident)
+
+## Funcionalidades
+
+ * Login de ONG
+ * Logout de ONG
+ * Cadastro de ONG
+ * Cadastrar novos casos
+ * Deletar casos
+ * Listar casos específicos de uma ONG
+ * Listar todos os casos
+ * Entrar em contato com a ONG
---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
+
+const app = express()
+
+// Desenvolvimento: Todo mundo acessa o back-end
+app.use(cors())	
+// Produção: Apenas a origin pode acessar o back-end
+app.use(cors({
+	origin: 'http:meuapp.com'
+}))
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
+
+/**
+ * Rota / Recurso
+ * 
+ * Métodos HTTP:
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ * 
+ * Tipos de parâmetros:
+ * Query Params: parâmetros nomeados enviados na rota após "?" (filtro, paginação)
+ * Route Params: parâmetros utilizados para identificar recursos
+ * Request Body: corpo da requisição, utilizado para criar ou alterar recursos
+ * 
+ * Bancos de Dados
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsot SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ * 
+ * Instalar Driver: SELECT * FROM users
+ * Instalar Query Builder: table('users').select('*').where()
+ */
